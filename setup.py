94042cc9226efea47ec794fa0a50c2b74dc9fbd7
Stefaan Lippens <soxofaan@gmail.com>
2017-12-19 13:39:15 +0100

    Change version to '0.21.38-mm' to be compatible with semantic versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'dumbo',
-    version = 'mm-0.21.38',
+    version = '0.21.38-mm',
     author = 'Klaas Bosteels',
     license = 'Apache Software License (ASF)',
     packages = find_packages(),
